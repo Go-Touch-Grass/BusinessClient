bedc0197c8fad85da2a491a6b974c8c64c221a59
KEEWEIKUANG <timespaceptld@hotmail.com>
2024-09-11 16:15:24 +0800

    Used shadcn component {error not fixed}
    Creating components for navbar footer etcetc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
-export default function Index() {
-  return <div>this is the main page</div>;
+import {Button} from "@/components/components/button"
+
+function HomePage() {
+  return <div>
+    <h1 className='text-3xl'>HomePage</h1>
+    <Button variant='outline' size='lg' className = "capitalize m-8">
+      Click me
+    </Button>
+  </div>
 }
+export default HomePage;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,7 +1,27 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import Container from '@/components/components/global/Container';
+import Navbar from '@/components/components/navbar/Navbar';
+const inter = Inter({ subsets: ['latin'] });
+
+
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}): React.ReactElement {
-  return <div>{children}</div>;
+}>) {
+  return (
+    
+      <html lang='en' suppressHydrationWarning>
+        <body className={inter.className}>
+          
+            <Navbar />
+            <Container>{children}</Container>
+          
+        </body>
+      </html>
+    
+  );
 }
