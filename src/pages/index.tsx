7efcd0e2a1311d32d36b9ee2501a245394491fe7
jy <Ninja514@yahoo.com>
2024-11-12 15:55:54 +0800

    Fixed Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ function AboutPage() {
       <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl text-green-900'>
         We love
         <span className='bg-green-500 py-2 px-4 rounded-lg tracking-widest text-white'>
-          GO TO GRASS
+          GO TOUCH GRASS
         </span>
       </h1>
       <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl text-center text-green-800'>
